5259a7c3e8e3f42e65224e6655b707da4aa0c83e
gamernishant <official007nishant@gmail.com>
2023-08-01 18:23:38 +0530

    Lecture-2

---
+++ Lecture-2/datatype.js
@@ -0,0 +1,37 @@
+// string
+let str = ''
+let str1 = 'A'
+let str2 = 'Hello'
+let str3 = "Stranger"
+
+console.log(typeof str);
+console.log(typeof str1);
+console.log(typeof str2);
+console.log(typeof str3);
+
+// number
+let n1 = 10;
+let n2 = 10.11
+
+console.log(typeof n1);
+console.log(typeof n2);
+
+// boolean
+let b1 = true;
+let b2 = false;
+
+console.log(typeof b1);
+console.log(typeof b2);
+
+
+// undefined
+console.log(d);
+var d;
+console.log(typeof d);
+// functions
+function print(){
+    console.log("Hello World");
+}
+console.log(typeof print);
+
+// object
---
+++ Lecture-2/file.js
@@ -0,0 +1,36 @@
+// var, let, const
+
+// const : constant variable
+
+// keyword variable_name = value;
+"use strict"
+
+var namel = "Hello";
+console.log(namel);
+
+let anotherName = "Stranger";
+console.log(anotherName);
+
+const c = 10;
+console.log(c);
+// c = 11;
+// console.log(c);
+
+if(true)
+{
+    var d = 100;
+    let e = 101;
+    //f = 112;
+    // c = 123;
+}
+
+console.log(d);
+// console.log(e);
+//console.log(f);
+// console.log(c);
+
+// hoisting
+
+
+console.log(m);
+var m;
